9b51a6713fa76d8ba8575776eaa6d364705fb35e
Pushkar Laulkar <pushkar.laulkar@yahoo.com>
2025-04-01 17:12:28 +0400

    Added comments & load balancer config to the ecs service

--- terraform/modules/ecs-task/main.tf
+++ terraform/modules/ecs-task/main.tf
@@ -1,3 +1,6 @@
+# Creating an ECS Task of type FARGATE on the cluster and task definition created in the previous modules.
+# Also configuring a load balancer to send traffic to this task
+
 resource "aws_ecs_service" "ecs_run_task" {
   name            = "simple-time-service"
   cluster         = var.ecs_cluster_id
@@ -9,5 +12,11 @@ resource "aws_ecs_service" "ecs_run_task" {
     subnets          = var.private_subnets
     security_groups  = [var.security_group_id]
     assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.sts_target_group
+    container_name   = "simple-time-service-container"
+    container_port   = 80
   }
 }
