bdb711f45a645db41d1bc7ef9ef358f414f39f0d
Pushkar Laulkar <pushkar.laulkar@yahoo.com>
2025-04-01 18:28:35 +0400

    Added X-Forwarded-For header to preserve client IP

--- app/server.py
+++ app/server.py
@@ -1,19 +1,17 @@
-# Import the main flask modules and other mandatory & preferred module
 from flask import Flask, jsonify, request
 from datetime import datetime
 
-#Initializing the app
 app = Flask(__name__)
 
-#Default route
 @app.route('/')
 def index():
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
+    
     response = {
-        "Time" : datetime.utcnow().strftime('%d %b %Y %I:%M %p'),
-        "Client IP" : request.remote_addr
+        "Time": datetime.utcnow().strftime('%d %b %Y %I:%M %p'),
+        "Client IP": client_ip
     }
     return jsonify(response)
 
-#Calling the app, debug option will make sure that app reloads everytime some code is changed
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
